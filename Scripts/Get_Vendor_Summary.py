456bca8dc03902d6cb440b23bc46e839fb8594dd
KusumaShetty Chinmay Karthikeya <karthikeyachinmay@gmail.com>
2025-08-10 18:38:10 +0530

    Vendor Project

---
+++ Scripts/Get_Vendor_Summary.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:91d0fb32daf43491f1d3cdaca5302c6da87c03580dc96ea5c0364c58e711f5d7
+size 4413
---
+++ Scripts/ingestion_db.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ae6a87bf70f68dc77b8c5f4cf0c3990a048c9312288bc5aaa71c9b6a9bfc0805
+size 986
